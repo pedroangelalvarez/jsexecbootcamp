aec51779dae2ff23b6266895409968c3d2f4be19
pedroangelalvarez <pedroangel.alvarez24@gmail.com>
2023-01-19 18:01:04 -0500

    Use shadow dom

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,77 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	let root;
+	let text;
+	let value;
+	let shadowRoot;
+	
+  
+	onMount(() => {
+	   const nd = root.querySelectorAll('.forminput');
+	   console.log(nd);
+	   shadowRoot = root.attachShadow({ mode: "open" });
+	   shadowRoot.innerHTML = `
+			<style>
+				ul {
+					list-style: none;
+					padding: 0;
+					margin: 0;
+				}
+				li {
+					display: inline-block;
+					margin: 0 0.5em;
+				}
+			</style>
+			<div id="shadow">
+				<p>Lenguajes de programación que dominas</p>
+				<div id="listLanguages">
+				<ul>
+					<li>
+						<input type="checkbox" id="language-1" name="language" value="JavaScript">
+						<label for="language-1">JavaScript</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-2" name="language" value="Python">
+						<label for="language-2">Python</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-3" name="language" value="Java">
+						<label for="language-3">Java</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-4" name="language" value="C++">
+						<label for="language-4">C++</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-5" name="language" value="C#">
+						<label for="language-5">C#</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-6" name="language" value="PHP">
+						<label for="language-6">PHP</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-7" name="language" value="Ruby">
+						<label for="language-7">Ruby</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-8" name="language" value="Swift">
+						<label for="language-8">Swift</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-9" name="language" value="Go">
+						<label for="language-9">Go</label>
+					</li>
+					<li>
+						<input type="checkbox" id="language-10" name="language" value="R">
+						<label for="language-10">R</label>
+					</li>
+					</ul>
+				</div>
+			</div>
+		`;
+	});
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  
+<div id="root" bind:this={root}>
+</div>
